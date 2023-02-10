106a5049f908719568112e7990ec948144ebb483
German <gtumorin@gmail.com>
2023-02-10 12:17:03 +0100

    check list of services for order form

--- src/components/personal_account/PersonalAccountMain.jsx
+++ src/components/personal_account/PersonalAccountMain.jsx
@@ -9,11 +9,11 @@ import PersonalAccountMenu from "./PersonalAccountMenu";
 const PersonalAccountMain = () => {
   const [date, setDate] = useState(new Date());
   const [servicesList, setServicesList] = useState([]);
+  const [operationsList, setOperationsList] = useState([]);
+  const [checkListState, setCheckListState] = useState();
 
   const dispatch = useDispatch();
-  const { services, isLoaded, fetchStatus } = useSelector(
-    (state) => state.services
-  );
+  const { services, isLoaded } = useSelector((state) => state.services);
 
   const getServices = async () => {
     try {
@@ -28,6 +28,18 @@ const PersonalAccountMain = () => {
     }
   };
 
+  const handleChange = (event) => {
+    setCheckListState(event.target.value);
+    console.log(checkListState);
+    const currService = servicesList.filter(
+      (service) => service.title === checkListState
+    );
+    if (currService.length > 0) {
+      setOperationsList(currService[0].services);
+    }
+    console.log(operationsList);
+  };
+
   useEffect(() => {
     getServices();
   }, [isLoaded]);
@@ -40,6 +52,22 @@ const PersonalAccountMain = () => {
         </div>
         <div className="content">
           <Form>
+            <div className="check-services_container">
+              <Form.Select
+                size="lg"
+                value={checkListState}
+                onChange={handleChange}
+              >
+                <option>Wybierz usługę</option>
+                {servicesList.map((service, index) => {
+                  return (
+                    <option key={index} value={service.title}>
+                      {service.title}
+                    </option>
+                  );
+                })}
+              </Form.Select>
+            </div>
             <div className="calendar-container">
               <Calendar
                 onChange={setDate}
