df760593b75f2e0001ced1d4b9edcc90c00e425b
German <gtumorin@gmail.com>
2023-02-10 18:19:38 +0100

    update selecting services

--- src/components/personal_account/PersonalAccountMain.jsx
+++ src/components/personal_account/PersonalAccountMain.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Form } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 import Calendar from "react-calendar";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -10,7 +10,10 @@ const PersonalAccountMain = () => {
   const [date, setDate] = useState(new Date());
   const [servicesList, setServicesList] = useState([]);
   const [operationsList, setOperationsList] = useState([]);
-  const [checkListState, setCheckListState] = useState();
+  const [selectedServiceState, setSelectedServiceState] = useState({
+    isSelected: false,
+    selectedService: "",
+  });
 
   const dispatch = useDispatch();
   const { services, isLoaded } = useSelector((state) => state.services);
@@ -29,20 +32,25 @@ const PersonalAccountMain = () => {
   };
 
   const handleChange = (event) => {
-    setCheckListState(event.target.value);
-    console.log(checkListState);
-    const currService = servicesList.filter(
-      (service) => service.title === checkListState
-    );
-    if (currService.length > 0) {
-      setOperationsList(currService[0].services);
-    }
-    console.log(operationsList);
+    console.log(event.target.value);
+    setSelectedServiceState({
+      isSelected: true,
+      selectedService: event.target.value,
+    });
   };
 
   useEffect(() => {
     getServices();
   }, [isLoaded]);
+
+  useEffect(() => {
+    const currService = servicesList.filter(
+      (service) => service.title === selectedServiceState.selectedService
+    );
+    if (currService.length > 0) {
+      setOperationsList(currService[0].services);
+    }
+  }, [selectedServiceState]);
 
   return (
     <div className="container">
@@ -52,34 +60,44 @@ const PersonalAccountMain = () => {
         </div>
         <div className="content">
           <Form>
-            <div className="check-services_container">
-              <Form.Select
-                size="lg"
-                value={checkListState}
-                onChange={handleChange}
-              >
-                <option>Wybierz usługę</option>
-                {servicesList.map((service, index) => {
-                  return (
-                    <option key={index} value={service.title}>
-                      {service.title}
-                    </option>
-                  );
-                })}
-              </Form.Select>
-            </div>
-            <div className="calendar-container">
-              <Calendar
-                onChange={setDate}
-                value={date}
-                minDate={new Date()}
-                minDetail={"month"}
-                locale={"pl-PL"}
-              />
-              <p className="calendar_text">
-                <span className="bold">Selected Date:</span>{" "}
-                {date.toDateString()}
-              </p>
+            <div className="common_container">
+              <div className="check-services_container">
+                <Form.Select
+                  size="lg"
+                  value={selectedServiceState.selectedService}
+                  onChange={handleChange}
+                >
+                  <option>Wybierz usługę</option>
+                  {servicesList.map((service, index) => {
+                    return (
+                      <option key={index} value={service.title}>
+                        {service.title}
+                      </option>
+                    );
+                  })}
+                </Form.Select>
+                <div className="operations">
+                  {selectedServiceState.isSelected &&
+                  selectedServiceState.selectedService !== "Wybierz usługę"
+                    ? operationsList.map(({ id, name }) => (
+                        <Button key={id}>{name}</Button>
+                      ))
+                    : null}
+                </div>
+              </div>
+              <div className="calendar-container">
+                <Calendar
+                  onChange={setDate}
+                  value={date}
+                  minDate={new Date()}
+                  minDetail={"month"}
+                  locale={"pl-PL"}
+                />
+                <p className="calendar_text">
+                  <span className="bold">Selected Date:</span>{" "}
+                  {date.toDateString()}
+                </p>
+              </div>
             </div>
           </Form>
         </div>
